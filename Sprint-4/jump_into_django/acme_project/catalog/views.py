72cbdf7a66057fca18618237af036bf80cc04f2f
BEZBIG <bezbig@yandex.ru>
2025-08-04 21:02:01 +0300

    Update urls and views

--- Sprint-4/jump_into_django/acme_project/catalog/views.py
+++ Sprint-4/jump_into_django/acme_project/catalog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def product_category(request, category):    
+    return HttpResponse(f'Категория {category}') 
--- Sprint-4/jump_into_django/acme_project/homepage/views.py
+++ Sprint-4/jump_into_django/acme_project/homepage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Класс HttpResponse нужно импортировать в код из модуля django.http.
+from django.http import HttpResponse
+
+def index(request):    
+    return HttpResponse('Главная страница')
