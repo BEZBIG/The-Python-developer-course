c494f5b9a637ae27dcd125e2248c010825e34adc
BEZBIG <bezbig@yandex.ru>
2025-08-19 15:13:19 +0200

    Update db_video_2.py and db_video_2.sqlite

--- Sprint-5/db_video_2.py
+++ Sprint-5/db_video_2.py
@@ -14,13 +14,20 @@ original_titles = [
 ]
 
 video_products = [
-    (1, 'Безумные мелодии Луни Тюнз', 3),
-    (2, 'Весёлые мелодии', 6),
-    (3, 'Кто подставил кролика Роджера', 5),
-    (4, 'Хороший, плохой, злой', 4),
-    (5, 'Последний киногерой', 1),
-    (6, 'Она написала убийство', 2),
-    (7, 'Миссис Харрис едет в Париж', 7)
+    (1, 'Безумные мелодии Луни Тюнз', 3, 2),
+    (2, 'Весёлые мелодии', 6, 2),
+    (3, 'Кто подставил кролика Роджера', 5, 1),
+    (4, 'Хороший, плохой, злой', 4, 3),
+    (5, 'Последний киногерой', 1, 3),
+    (6, 'Она написала убийство', 2, 4),
+    (7, 'Миссис Харрис едет в Париж', 7, 3)
+]
+
+product_types = [
+    (1, 'Мультфильм'),
+    (2, 'Мультсериал'),
+    (3, 'Фильм'),
+    (4, 'Сериал'),
 ]
 
 query = '''
@@ -29,17 +36,25 @@ CREATE TABLE IF NOT EXISTS original_titles(
     title TEXT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS product_types(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS video_products(
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     original_title_id INTEGER NOT NULL UNIQUE,
-    FOREIGN KEY(original_title_id) REFERENCES original_titles(id)
+    type_id INTEGER NOT NULL,
+    FOREIGN KEY(original_title_id) REFERENCES original_titles(id),
+    FOREIGN KEY(type_id) REFERENCES product_types(id)
 );
 '''
 
 cur.executescript(query)
 cur.executemany('INSERT INTO original_titles VALUES(?, ?);', original_titles)
-cur.executemany('INSERT INTO video_products VALUES(?, ?, ?);', video_products)
+cur.executemany('INSERT INTO product_types VALUES(?, ?);', product_types)
+cur.executemany('INSERT INTO video_products VALUES(?, ?, ?, ?);', video_products)
 
 con.commit()
 
@@ -56,5 +71,18 @@ WHERE video_products.original_title_id = original_titles.id
 
 for result in results:
     print(result)
-    
-con.close() 
+
+
+print("\n")
+
+results_2 = cur.execute('''
+SELECT video_products.title,
+       product_types.name
+FROM video_products,
+     product_types
+WHERE video_products.type_id = product_types.id AND product_types.name = 'Фильм'; 
+''')
+
+for result in results_2:
+    print(result)
+con.close()
