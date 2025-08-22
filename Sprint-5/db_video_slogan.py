7ba0a81832550d88017749aa5c63dc28a5f5aeb0
BEZBIG <bezbig@yandex.ru>
2025-08-22 11:30:16 +0200

    Add Task 4:db_video_slogan.py

--- Sprint-5/db_video_slogan.py
+++ Sprint-5/db_video_slogan.py
@@ -3,13 +3,26 @@ con = sqlite3.connect('db_video_type_slogan.sqlite')
 cur = con.cursor()
 results = cur.execute('''
     SELECT video_products.title,
-       slogans.slogan_text,
-       product_types.title
+       slogans.slogan_text
     FROM video_products
-    JOIN slogans ON video_products.slogan_id = slogans.id
-    JOIN product_types ON video_products.type_id = product_types.id; 
+    LEFT JOIN slogans ON video_products.slogan_id = slogans.id
+    UNION
+    SELECT video_products.title,
+        slogans.slogan_text
+    FROM slogans
+    LEFT JOIN video_products ON video_products.slogan_id = slogans.id; 
 ''')
 for result in results:
     print(result)
 
+print('==='*10)
+
+results = cur.execute('''
+    SELECT video_products.title,
+       slogans.slogan_text
+    FROM video_products
+    CROSS JOIN slogans; 
+''')
+for result in results:
+    print(result)
 con.close() 
