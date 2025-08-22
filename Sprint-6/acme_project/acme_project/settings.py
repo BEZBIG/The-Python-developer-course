04d4ab062caa4593ec47a7466032d67be04f71b4
BEZBIG <bezbig@yandex.ru>
2025-08-22 12:34:10 +0200

    Update settings.py, add OriginalTitle and VideoProduct models

--- Sprint-6/acme_project/acme_project/settings.py
+++ Sprint-6/acme_project/acme_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "catalog.apps.CatalogConfig",
-    "homepage.apps.HomepageConfig"
+    "homepage.apps.HomepageConfig",
+    "cinema.apps.CinemaConfig"
 ]
 
 MIDDLEWARE = [
--- Sprint-6/acme_project/cinema/models.py
+++ Sprint-6/acme_project/cinema/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class OriginalTitle(models.Model):
+    title = models.CharField(max_length=128)
+
+class VideoProduct(models.Model):
+    title = models.CharField(max_length=128)
+    original_title = models.OneToOneField(
+        OriginalTitle,
+        on_delete=models.SET_NULL,
+        null=True
+    )
