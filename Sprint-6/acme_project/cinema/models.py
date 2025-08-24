2e1355557da627104bf000aed6f4d8dc0fa03ef7
BEZBIG <bezbig@yandex.ru>
2025-08-24 11:10:07 +0200

    Update VideoProduct model and add Director and DirectorVideoProduct models

--- Sprint-6/acme_project/cinema/models.py
+++ Sprint-6/acme_project/cinema/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class OriginalTitle(models.Model):
     title = models.CharField(max_length=128)
 
+class ProductType(models.Model):
+    title = models.CharField(max_length=128)
+
 class VideoProduct(models.Model):
     title = models.CharField(max_length=128)
     original_title = models.OneToOneField(
@@ -10,3 +13,19 @@ class VideoProduct(models.Model):
         on_delete=models.SET_NULL,
         null=True
     )
+
+    directors = models.ManyToManyField(
+        'Director'
+    )
+
+    product_type = models.ForeignKey(
+        ProductType,
+        on_delete=models.CASCADE,
+    )
+
+class Director(models.Model):
+    full_name = models.CharField(max_length=128)
+
+class DirectorVideoProduct(models.Model):
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+    video_product = models.ForeignKey(VideoProduct, on_delete=models.CASCADE)
