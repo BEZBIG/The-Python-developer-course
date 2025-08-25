c7033a19f4f69b04ccc9ffa5c4cf80f2ced6556f
BEZBIG <bezbig@yandex.ru>
2025-08-25 13:00:55 +0200

    Update views.py:icecream

--- Sprint-6/anfisa2sprint/anfisa_for_friends/homepage/views.py
+++ Sprint-6/anfisa2sprint/anfisa_for_friends/homepage/views.py
@@ -1,18 +1,19 @@
+# Для применения Q-объектов их нужно импортировать:
+from django.db.models import Q
 from django.shortcuts import render
-from ice_cream.models import IceCream
 
+from ice_cream.models import IceCream
 
 def index(request):
     template_name = 'homepage/index.html'
-    # Заключаем вызов методов в скобки
-    # (это стандартный способ переноса длинных строк в Python);
-    # каждый вызов пишем с новой строки, так проще читать код:
     ice_cream_list = IceCream.objects.values(
-            'id', 'title', 'description'
-        # Верни только те объекты, у которых в поле is_on_main указано True:
-        ).filter(is_on_main=True)
+        'id', 'title', 'description'
+    ).filter(
+        # Делаем запрос, объединяя два условия
+        # через Q-объекты и оператор AND:
+        Q(is_published=True) & Q(is_on_main=True)
+    )
     context = {
         'ice_cream_list': ice_cream_list,
     }
-    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
     return render(request, template_name, context) 
