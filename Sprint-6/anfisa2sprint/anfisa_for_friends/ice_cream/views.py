1becc59ab0f2132b1331751dc263e5c8e6ba2a6d
BEZBIG <bezbig@yandex.ru>
2025-08-27 10:48:22 +0200

    Update views.py:icecream

--- Sprint-6/anfisa2sprint/anfisa_for_friends/ice_cream/views.py
+++ Sprint-6/anfisa2sprint/anfisa_for_friends/ice_cream/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
+from ice_cream.models import IceCream
 
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
-    context = {}
+     # Вызываем .get() и в его параметрах указываем условия фильтрации:
+    ice_cream = IceCream.objects.get(pk=pk)
+    context = {
+        'ice_cream': ice_cream,
+    }
     return render(request, template, context)
 
 
